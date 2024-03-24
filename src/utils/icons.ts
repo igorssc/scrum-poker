db8988fe5a4c07467aa29acaf2dabb1f687c4989
Igor Costa <igorsantoscosta@gmail.com>
2024-03-24 00:33:02 -0300

    feat: create icons data file

---
+++ src/utils/icons.ts
@@ -0,0 +1,24 @@
+export const iconsData = [
+  {
+    name: 'Natureza',
+    verseIcon: 'nature/verse.svg',
+    prevIcons: [
+      'cake/cake.svg',
+      'dragons/cake.svg',
+      'pause/cake.svg',
+      'shaving/cake.svg',
+    ],
+    icons: [
+      'nature/1.svg',
+      'nature/2.svg',
+      'nature/3.svg',
+      'nature/5.svg',
+      'nature/8.svg',
+      'nature/13.svg',
+      'nature/20.svg',
+      'nature/40.svg',
+      'nature/100.svg',
+    ],
+    latterIcons: ['nature/infinite.svg'],
+  },
+];
