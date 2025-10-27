0d4b57fded5f389d2080262e329230347e369f5a
Igor Costa <igorsantoscosta@gmail.com>
2025-10-27 17:08:17 -0300

    refactor(Board): reorganize imports and improve Box component structure for better readability

--- src/components/Board.tsx
+++ src/components/Board.tsx
@@ -1,16 +1,16 @@
-import { useContextSelector } from 'use-context-selector';
 import { RoomContext } from '@/context/RoomContext';
 import { useRoomActions } from '@/hooks/useRoomActions';
-import { AcceptUsers } from './AcceptUsers';
-import { useQueryClient } from '@tanstack/react-query';
 import { MemberProps } from '@/protocols/Member';
 import { RoomProps } from '@/protocols/Room';
-import { UsersList } from './UsersList';
-import { NavBar } from './NavBar';
+import { useQueryClient } from '@tanstack/react-query';
+import { useContextSelector } from 'use-context-selector';
+import { AcceptUsers } from './AcceptUsers';
 import { Box } from './Box';
+import { Button } from './Button';
 import { Cards } from './Cards';
 import { Flex } from './Flex';
-import { Button } from './Button';
+import { NavBar } from './NavBar';
+import { UsersList } from './UsersList';
 
 export const Board = () => {
   const { room, user } = useContextSelector(RoomContext, context => ({
@@ -34,29 +34,31 @@ export const Board = () => {
       <main className="flex flex-col gap-3 sm:gap-4 md:gap-6">
         {(!data?.data.private || room?.owner_id === user?.id) && <AcceptUsers />}
 
-        <Box className="max-w-full min-h-0!">
+        <Box className="max-w-full min-h-0! flex flex-col gap-y-3 sm:gap-y-4 md:gap-y-6 lg:gap-y-8">
           <Cards />
+
+          {isOwner && (
+            <Flex className="w-full flex-row gap-2 md:gap-4">
+              <Button
+                className="flex-1"
+                onClick={clearVotes}
+                isLoading={isClearingVotes}
+                variant="tertiary"
+              >
+                Limpar votos
+              </Button>
+              <Button
+                className="flex-1"
+                onClick={revealCards}
+                isLoading={isRevealingCards}
+                variant="tertiary"
+              >
+                Revelar cartas
+              </Button>
+            </Flex>
+          )}
         </Box>
-        {isOwner && (
-          <Flex className="w-full flex-row gap-2 md:gap-4">
-            <Button
-              className="flex-1"
-              onClick={clearVotes}
-              isLoading={isClearingVotes}
-              variant="tertiary"
-            >
-              Limpar votos
-            </Button>
-            <Button
-              className="flex-1"
-              onClick={revealCards}
-              isLoading={isRevealingCards}
-              variant="tertiary"
-            >
-              Revelar cartas
-            </Button>
-          </Flex>
-        )}
+
         <UsersList />
       </main>
     </div>
