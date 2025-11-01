c4e1e7ebfd086c051758963430337587e60571d1
Igor Costa <igorsantoscosta@gmail.com>
2025-11-01 15:16:10 -0300

    feat: add new properties to RoomProps type for enhanced room permissions management

--- src/protocols/Room.ts
+++ src/protocols/Room.ts
@@ -12,4 +12,7 @@ export type RoomProps = {
   status: 'OPEN' | 'CLOSED';
   theme: IconThemeData;
   cards_open: boolean;
+  who_can_edit: string[];
+  who_can_open_cards: string[];
+  who_can_aprove_entries: string[];
 };
